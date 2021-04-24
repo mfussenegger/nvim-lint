16493a15f3876db2b46dc1e821e63b061060860d
Pavit Kiatkraipob <pavit.kiat@gmail.com>
2021-04-24 22:32:00 +0700

    feat: add golangci-lint linter

---
+++ lua/lint/linters/golangcilint.lua
@@ -0,0 +1,50 @@
+local severities = {
+  error = vim.lsp.protocol.DiagnosticSeverity.Error,
+  warning = vim.lsp.protocol.DiagnosticSeverity.Warning,
+  refactor = vim.lsp.protocol.DiagnosticSeverity.Information,
+  convention = vim.lsp.protocol.DiagnosticSeverity.Hint,
+}
+
+return {
+  cmd = 'golangci-lint',
+  stdin = true,
+  args = {
+    'run',
+    '--out-format',
+    'json',
+  },
+  stream = 'stdout',
+  ignore_exitcode = true,
+  parser = function(output, bufnr)
+    if output == '' then
+      return {}
+    end
+    local decoded = vim.fn.json_decode(output)
+    if decoded["Issues"] == nil or type(decoded["Issues"]) == 'userdata' then
+      return {}
+    end
+
+    local diagnostics = {}
+    for _, item in ipairs(decoded["Issues"]) do
+      local sv = vim.lsp.protocol.DiagnosticSeverity.Warning
+      if severities[item.Severity] ~= nil then
+        sv = severities[item.Severity]
+      end
+      table.insert(diagnostics, {
+        range = {
+          ['start'] = {
+            line = item.Pos.Line - 1,
+            character = item.Pos.Column,
+          },
+          ['end'] = {
+            line = item.Pos.Line - 1,
+            character = item.Pos.Column + 1,
+          },
+        },
+        severity = sv,
+        message = item.Text,
+      })
+    end
+    return diagnostics
+  end
+}
