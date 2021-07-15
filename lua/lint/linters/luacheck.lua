f745eae86b6cce4e492a8ba5f6ecfbf1a7964b6b
Gregory Anders <greg@gpanders.com>
2021-07-15 14:39:29 -0600

    luacheck: add vim global when checking runtime files
    If the current buffer is modifying a Lua file found underneath an entry
    in &runtimepath then add a read-only global 'vim'.
    
    This suppresses the "undefined value" warnings in Lua files used from
    Neovim.

--- lua/lint/linters/luacheck.lua
+++ lua/lint/linters/luacheck.lua
@@ -5,37 +5,50 @@ local severities = {
 
 local pattern = '[^:]+:(%d+):(%d+)-(%d+): %((%a)(%d+)%) (.*)'
 
-return {
-    cmd = 'luacheck',
-    stdin = true,
-    args = {'--formatter', 'plain', '--codes', '--ranges', '-'},
-    ignore_exitcode = true,
-    parser = function(output, _)
-        local result = vim.fn.split(output, "\n")
-        local diagnostics = {}
-
-        for _, message in ipairs(result) do
-            local line, offs, offe, severity, code, desc =
-                string.match(message, pattern)
-
-            line = tonumber(line or 1) - 1
-            offs = tonumber(offs or 1) - 1
-            offe = tonumber(offe or 1)
-
-            table.insert(diagnostics, {
-                source = 'luacheck',
-                range = {
-                    ['start'] = {line = line, character = offs},
-                    ['end'] = {line = line, character = offe}
-                },
-                message = desc,
-                severity = assert(severities[severity],
-                                  'missing mapping for severity ' .. severity),
-                code = code
-            })
+return function()
+    local args = {'--formatter', 'plain', '--codes', '--ranges', '-'}
+    local bufnr = vim.api.nvim_get_current_buf()
+    local filename = vim.api.nvim_buf_get_name(bufnr)
+    for path in vim.gsplit(vim.o.runtimepath, ',') do
+        if filename:find('^' .. path) then
+            table.insert(args, '--read-globals')
+            table.insert(args, 'vim')
+            break
         end
-
-        return diagnostics
     end
-}
+
+    return {
+        cmd = 'luacheck',
+        stdin = true,
+        args = args,
+        ignore_exitcode = true,
+        parser = function(output, _)
+            local result = vim.fn.split(output, "\n")
+            local diagnostics = {}
+
+            for _, message in ipairs(result) do
+                local line, offs, offe, severity, code, desc =
+                    string.match(message, pattern)
+
+                line = tonumber(line or 1) - 1
+                offs = tonumber(offs or 1) - 1
+                offe = tonumber(offe or 1)
+
+                table.insert(diagnostics, {
+                    source = 'luacheck',
+                    range = {
+                        ['start'] = {line = line, character = offs},
+                        ['end'] = {line = line, character = offe}
+                    },
+                    message = desc,
+                    severity = assert(severities[severity],
+                                      'missing mapping for severity ' .. severity),
+                    code = code
+                })
+            end
+
+            return diagnostics
+        end
+    }
+end
 
