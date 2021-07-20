038c81f19b147b604e7b7f34a40f63041d909669
Luc Sinet <luc.sinet@gmail.com>
2021-07-20 09:38:08 +0200

    Configure the eslint parser

---
+++ lua/lint/linters/eslint.lua
@@ -0,0 +1,14 @@
+local pattern = [[%s*(%d+):(%d+)%s+(%w+)%s+([%w%s]+)%s+(.*)]]
+local groups = { 'line', 'start_col', 'severity', 'message', 'code' }
+local severity_map = {
+  ['error'] = vim.lsp.protocol.DiagnosticSeverity.Error,
+  ['warn'] = vim.lsp.protocol.DiagnosticSeverity.Warning,
+}
+
+return {
+  cmd = 'eslint',
+  args = {},
+  stdin = false,
+  stream = 'stdout',
+  parser = require('lint.parser').from_pattern(pattern, groups, severity_map, { ['source'] = 'eslint' }),
+}
