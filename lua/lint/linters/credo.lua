6b7e4d57f1da1448b3ddd361a6adc10a9f83a25f
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-08-14 13:49:05 -0300

    fix: parser definition

--- lua/lint/linters/credo.lua
+++ lua/lint/linters/credo.lua
@@ -1,8 +1,8 @@
 return {
-  cmd = 'mix credo',
-  args = { '--strict' },
+  cmd = 'mix',
+  args = { 'credo', '--strict' },
   ignore_exitcode = false,
-  parser = require('lint-parser').from_errorformat('%f:%l: %m', {
+  parser = require('lint.parser').from_errorformat('%f:%l: %m', {
     source = 'mix credo',
     severity = vim.lsp.protocol.DiagnosticSeverity.Information
   })
