c9bff647b41d522d1824f7218c66fbb270bb075d
LongerHV <michal@mieszczak.com.pl>
2021-08-27 16:07:38 +0200

    Enable stdin for flake8

--- lua/lint/linters/flake8.lua
+++ lua/lint/linters/flake8.lua
@@ -4,10 +4,11 @@ local groups = { 'line', 'start_col', 'code', 'message' }
 
 return {
   cmd = 'flake8',
-  stdin = false,
+  stdin = true,
   args = {
     '--format=%(path)s:%(row)d:%(col)d:%(code)s:%(text)s',
     '--no-show-source',
+    '-',
   },
   ignore_exitcode = true,
   parser = require('lint.parser').from_pattern(pattern, groups, nil, {
