4b6b0477ce552da3a2daabc845fbc1229cc9e8a4
Mathias Fussenegger <f.mathias@zignar.net>
2021-08-31 18:11:07 +0200

    Use global makeprg/errorformat as fallback in compiler linter

--- lua/lint/linters/compiler.lua
+++ lua/lint/linters/compiler.lua
@@ -2,8 +2,15 @@ local api = vim.api
 
 
 return function()
-  local errorformat = api.nvim_buf_get_option(0, 'errorformat')
-  local makeprg = api.nvim_buf_get_option(0, 'makeprg')
+  local ok, errorformat = pcall(api.nvim_buf_get_option, 0, 'errorformat')
+  if not ok then
+    errorformat = vim.o.errorformat
+  end
+  local makeprg
+  ok, makeprg = pcall(api.nvim_buf_get_option, 0, 'makeprg')
+  if not ok then
+    makeprg = vim.o.makeprg
+  end
   local bufname = api.nvim_buf_get_name(0)
   local args = {
     api.nvim_get_option('shellcmdflag'),
