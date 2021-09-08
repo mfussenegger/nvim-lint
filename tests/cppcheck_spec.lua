11b3ee5be5995e58be32956dfb2b94c96d00b799
Tyler S. Jordan <tylerscottjordan@gmail.com>
2021-09-07 23:11:00 -0600

    Add cppcheck test. Need parser to allow foo.cpp.

---
+++ tests/cppcheck_spec.lua
@@ -0,0 +1,50 @@
+describe('linter.cppcheck', function()
+  it('can parse the output', function()
+    local parser = require('lint.linters.cppcheck').parser
+    local result = parser([[
+./foo.cpp:46:7: [unusedVariable] style: Unused variable: fd
+./foo.cpp:366:3: [postfixOperator] performance: Prefer prefix ++/-- operators for non-primitive types.
+./foo.cpp:46:{column}: [unusedVariable] style: Unused variable: fd
+./foo.cpp:366:{column}: [postfixOperator] performance: Prefer prefix ++/-- operators for non-primitive types.
+]])
+
+  assert.are.same(4, #result)
+
+  local expected = {
+    source = 'cppcheck',
+    code = 'unusedVariable',
+    message = 'Unused variable: fd',
+    range = {
+      ['start'] = {
+        character = 6,
+        line = 45,
+      },
+      ['end'] = {
+        character = 7,
+        line = 45,
+      }
+    },
+    severity = vim.lsp.protocol.DiagnosticSeverity.Information,
+  }
+  assert.are.same(expected, result[1])
+
+  local expected = {
+    source = 'cppcheck',
+    code = 'unusedVariable',
+    message = 'Unused variable: fd',
+    range = {
+      ['start'] = {
+        character = 0,
+        line = 45,
+      },
+      ['end'] = {
+        character = 1,
+        line = 45,
+      }
+    },
+    severity = vim.lsp.protocol.DiagnosticSeverity.Information,
+  }
+  assert.are.same(expected, result[3])
+
+  end)
+end)
