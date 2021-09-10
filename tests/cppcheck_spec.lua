e6109dcfc84d272d48f8a031d8421a2fc26cdf3f
Tyler S. Jordan <tylerscottjordan@gmail.com>
2021-09-09 19:17:46 -0600

    Give cppcheck buffer filename. Tests pass

--- tests/cppcheck_spec.lua
+++ tests/cppcheck_spec.lua
@@ -1,16 +1,17 @@
 describe('linter.cppcheck', function()
   it('can parse the output', function()
     local parser = require('lint.linters.cppcheck').parser
+    local bufnr = vim.uri_to_bufnr('file:///foo.cpp')
     local result = parser([[
-./foo.cpp:46:7: [unusedVariable] style: Unused variable: fd
-./foo.cpp:366:3: [postfixOperator] performance: Prefer prefix ++/-- operators for non-primitive types.
-./foo.cpp:46:{column}: [unusedVariable] style: Unused variable: fd
-./foo.cpp:366:{column}: [postfixOperator] performance: Prefer prefix ++/-- operators for non-primitive types.
-]])
+/foo.cpp:46:7: [unusedVariable] style: Unused variable: fd
+/foo.cpp:366:3: [postfixOperator] performance: Prefer prefix ++/-- operators for non-primitive types.
+/foo.cpp:46:{column}: [unusedVariable] style: Unused variable: fd
+/foo.cpp:366:{column}: [postfixOperator] performance: Prefer prefix ++/-- operators for non-primitive types.
+]], bufnr)
 
   assert.are.same(4, #result)
 
-  local expected = {
+  local expected_1_88 = {
     source = 'cppcheck',
     code = 'unusedVariable',
     message = 'Unused variable: fd',
@@ -26,9 +27,9 @@ describe('linter.cppcheck', function()
     },
     severity = vim.lsp.protocol.DiagnosticSeverity.Information,
   }
-  assert.are.same(expected, result[1])
+  assert.are.same(expected_1_88, result[1])
 
-  local expected = {
+  local expected_1_34 = {
     source = 'cppcheck',
     code = 'unusedVariable',
     message = 'Unused variable: fd',
@@ -44,7 +45,7 @@ describe('linter.cppcheck', function()
     },
     severity = vim.lsp.protocol.DiagnosticSeverity.Information,
   }
-  assert.are.same(expected, result[3])
+  assert.are.same(expected_1_34, result[3])
 
   end)
 end)
