fd30150859baad092ef86565f4b338061d8cfd27
Thanawat Yodnil <littleboycoding@gmail.com>
2021-09-18 02:08:33 +0700

    Added eslint_d

---
+++ lua/lint/linters/eslint_d.lua
@@ -0,0 +1,23 @@
+local function get_root (...)
+  local root_dir = require'lspconfig/util'.root_pattern(...)
+  local cwd = vim.fn.getcwd()
+  return root_dir(cwd)
+end
+
+local pattern = [[%s*(%d+):(%d+)%s+(%w+)%s+(.+%S)%s+(%S+)]]
+local groups = { 'line', 'start_col', 'severity', 'message', 'code' }
+local severity_map = {
+  ['error'] = vim.lsp.protocol.DiagnosticSeverity.Error,
+  ['warn'] = vim.lsp.protocol.DiagnosticSeverity.Warning,
+}
+
+local root_dir = get_root('package.json')
+
+return {
+  cmd = {root_dir .. '/node_modules/eslint_d/bin/eslint_d.js', 'eslint_d'},
+  args = {'--stdin'},
+  stdin = true,
+  stream = 'stdout',
+  ignore_exitcode = true,
+  parser = require('lint.parser').from_pattern(pattern, groups, severity_map, { ['source'] = 'eslint_d' }),
+}
