245a3bf8d689b179104dab6e19e08cf22d20aea2
Collin Peters <collin.peters@gmail.com>
2021-10-15 03:50:29 -0700

    Add checkstyle linter (#97)

---
+++ lua/lint/linters/checkstyle.lua
@@ -0,0 +1,23 @@
+local format = '[%tRROR] %f:%l: %m, [%tRROR] %f:%l:%c: %m, [%tARN] %f:%l:%c: %m, [%tARN] %f:%l: %m'
+
+local M
+
+local function config()
+  if M.config_file == nil then
+    error "Missing checkstyle config. e.g.: `require('lint.linters.checkstyle').config_file = '/path/to/checkstyle_config.xml'`"
+  end
+  return M.config_file
+end
+
+M = {
+  cmd = 'checkstyle',
+  args = {'-c', config},
+  ignore_exitcode = true,
+  parser = require('lint.parser').from_errorformat(format, {
+    source = 'checkstyle',
+  }),
+  -- use the bundled Google style by default
+  config_file = '/google_checks.xml'
+}
+
+return M
