6d3a8c3c22f186c91f83fbd8fd9f656b3828c4be
Derek Wan <d.wan@icloud.com>
2021-10-25 22:06:30 +0800

    Convert tab -> 2 space

--- lua/lint/linters/hlint.lua
+++ lua/lint/linters/hlint.lua
@@ -1,26 +1,26 @@
 local severities = {
-	error = vim.lsp.protocol.DiagnosticSeverity.Error,
-	warning = vim.lsp.protocol.DiagnosticSeverity.Warning,
-	suggestion = vim.lsp.protocol.DiagnosticSeverity.Hint,
+  error = vim.lsp.protocol.DiagnosticSeverity.Error,
+  warning = vim.lsp.protocol.DiagnosticSeverity.Warning,
+  suggestion = vim.lsp.protocol.DiagnosticSeverity.Hint,
 }
 
 return {
-	cmd = "hlint",
-	args = { "--json" },
-	parser = function(output)
-		local diagnostics = {}
-		local items = #output > 0 and vim.fn.json_decode(output) or {}
-		for _, item in ipairs(items) do
-			table.insert(diagnostics, {
-				range = {
-					["start"] = { line = item.startLine, character = item.startColumn },
-					["end"] = { line = item.endLine, character = item.endColumn },
-				},
-				severity = severities[item.severity:lower()],
-				source = "hlint",
-				message = item.hint,
-			})
-		end
-		return diagnostics
-	end,
+  cmd = "hlint",
+  args = { "--json" },
+  parser = function(output)
+    local diagnostics = {}
+    local items = #output > 0 and vim.fn.json_decode(output) or {}
+    for _, item in ipairs(items) do
+      table.insert(diagnostics, {
+        range = {
+          ["start"] = { line = item.startLine, character = item.startColumn },
+          ["end"] = { line = item.endLine, character = item.endColumn },
+        },
+        severity = severities[item.severity:lower()],
+        source = "hlint",
+        message = item.hint,
+      })
+    end
+    return diagnostics
+  end,
 }
