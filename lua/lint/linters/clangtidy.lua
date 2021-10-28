b1467a01f48736b309e0e274c032b69051740c08
patrick96 <p.ziegler96@gmail.com>
2021-10-28 21:44:28 +0200

    clangtidy: Handle 'note' messages as hints
    By default they were treated as errors

--- lua/lint/linters/clangtidy.lua
+++ lua/lint/linters/clangtidy.lua
@@ -1,9 +1,19 @@
 local pattern = [[([^:]*):(%d+):(%d+): (%w+): ([^[]+)]]
 local groups = { 'file', 'line', 'start_col', 'severity', 'message' }
 
+local DiagnosticSeverity = vim.lsp.protocol.DiagnosticSeverity
+
+local severity_map = {
+  ['error'] = DiagnosticSeverity.Error,
+  ['warning'] = DiagnosticSeverity.Warning,
+  ['information'] = DiagnosticSeverity.Information,
+  ['hint'] = DiagnosticSeverity.Hint,
+  ['note'] = DiagnosticSeverity.Hint,
+}
+
 return {
   cmd = 'clang-tidy',
   stdin = false,
   args = { '--quiet' },
-  parser = require('lint.parser').from_pattern(pattern, groups, nil, { ['source'] = 'clang-tidy' }),
+  parser = require('lint.parser').from_pattern(pattern, groups, severity_map, { ['source'] = 'clang-tidy' }),
 }
