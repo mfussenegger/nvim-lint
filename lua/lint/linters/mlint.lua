41ef9c145f527d4a174387fd08d7b31518358c7a
mendes-davi <dmendes@aluno.unb.br>
2021-11-05 00:38:11 -0300

    fix: line & column parsing for zero-index line or col.

--- lua/lint/linters/mlint.lua
+++ lua/lint/linters/mlint.lua
@@ -12,7 +12,7 @@ return {
   cmd = "mlint",
   stdin = false,
   stream = "stderr",
-  args = { "-cyc", "-id", "-severity" },
+  args = { "-cyc", "-id", "-severity", "-aa" },
   ignore_exitcode = true,
   parser = function(output, _)
     local result = vim.fn.split(output, "\n")
@@ -21,8 +21,10 @@ return {
     for _, line in ipairs(result) do
       local lineno, start_col, end_col, code, sev, desc = string.match(line, pattern)
 
-      lineno = tonumber(lineno or 0) - 1
-      start_col = tonumber(start_col or 0) - 1
+      lineno = tonumber(lineno or 1) - 1
+      lineno = lineno >= 0 and lineno or 0
+      start_col = tonumber(start_col or 1) - 1
+      start_col = start_col >= 0 and start_col or 0
       end_col = end_col and tonumber(end_col) or start_col + 1
       table.insert(diagnostics, {
         source = "mlint",
