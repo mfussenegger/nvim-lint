d871688d5dba4a0a50758d2d0b1a394f9113f038
mendes-davi <dmendes@aluno.unb.br>
2021-11-06 12:23:29 -0300

    chore: mlint parser using from_pattern

--- lua/lint/linters/mlint.lua
+++ lua/lint/linters/mlint.lua
@@ -1,4 +1,5 @@
 local pattern = "L (%d+) %(C (%d+)[-]?([%d]*)%): (.-): (.-): (.*)"
+local groups = { "line", "start_col", "end_col", "code", "severity", "message" }
 local severities = {
   ML2 = vim.lsp.protocol.DiagnosticSeverity.Error,
   ML3 = vim.lsp.protocol.DiagnosticSeverity.Error,
@@ -14,30 +15,5 @@ return {
   stream = "stderr",
   args = { "-cyc", "-id", "-severity" },
   ignore_exitcode = true,
-  parser = function(output, _)
-    local result = vim.fn.split(output, "\n")
-    local diagnostics = {}
-
-    for _, line in ipairs(result) do
-      local lineno, start_col, end_col, code, sev, desc = string.match(line, pattern)
-
-      lineno = tonumber(lineno or 1) - 1
-      lineno = lineno >= 0 and lineno or 0
-      start_col = tonumber(start_col or 1) - 1
-      start_col = start_col >= 0 and start_col or 0
-      end_col = end_col and tonumber(end_col) or start_col + 1
-      table.insert(diagnostics, {
-        source = "mlint",
-        range = {
-          ["start"] = { line = lineno, character = start_col },
-          ["end"] = { line = lineno, character = end_col },
-        },
-        message = desc,
-        severity = assert(severities[sev], "missing mapping for severity " .. sev),
-        code = code,
-      })
-    end
-    return diagnostics
-  end,
+  parser = require("lint.parser").from_pattern(pattern, groups, severities, { ["source"] = "mlint" }),
 }
-
