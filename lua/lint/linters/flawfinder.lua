8f96a16d2601deb4af8053132d8b23c67f1b690b
David Houston <houstdav000@gmail.com>
2021-11-07 06:42:27 -0500

    Add flawfinder (#113)
    Add the flawfinder linter for checking C and C++ programs for CWEs.
    
    Signed-off-by: David Houston <houstdav000@gmail.com>

---
+++ lua/lint/linters/flawfinder.lua
@@ -0,0 +1,26 @@
+-- Look for patterns like the following:
+--
+-- vuln.c:6:3:  [5] (buffer) gets:Does not check for buffer overflows (CWE-120, CWE-20).  Use fgets() instead.
+
+local pattern = [[^(.*):(%d+):(%d+): *%[([0-5])%] (.*)$]]
+local groups = { 'file', 'line', 'start_col', 'severity', 'message' }
+
+local DiagnosticSeverity = vim.lsp.protocol.DiagnosticSeverity
+
+local severity_map = {
+  ['5'] = DiagnosticSeverity.Warning,
+  ['4'] = DiagnosticSeverity.Warning,
+  ['3'] = DiagnosticSeverity.Warning,
+  ['2'] = DiagnosticSeverity.Warning,
+  ['1'] = DiagnosticSeverity.Warning,
+}
+
+return {
+  cmd = 'flawfinder',
+  stdin = false,
+  args = {'-S', '-Q', '-D', '-C', '--'},
+  stream = 'stdout',
+  parser = require('lint.parser').from_pattern(pattern, groups, severity_map, {
+    ['source'] = 'flawfinder'
+  })
+}
