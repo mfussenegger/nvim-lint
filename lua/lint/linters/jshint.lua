8bef724ccc51866274a67646673d537f2df82cdb
Mateus Etto <Yutsuten@users.noreply.github.com>
2021-11-25 04:42:38 +0900

    Add jshint (#118)

---
+++ lua/lint/linters/jshint.lua
@@ -0,0 +1,11 @@
+return {
+  cmd = 'jshint',
+  stdin = false,
+  args = {'--reporter', 'unix', '--extract', 'auto'},
+  stream = 'stdout',
+  ignore_exitcode = true,
+  parser = require('lint.parser').from_errorformat('%f:%l:%c: %m', {
+    source = 'jshint',
+    severity = vim.lsp.protocol.DiagnosticSeverity.Warning,
+  })
+}
