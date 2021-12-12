65ce0c16f5c56800bbbe7729d2e58edb28e10739
Mathias Fussenegger <f.mathias@zignar.net>
2021-12-12 17:02:07 +0100

    Default markdownlint cmd to `markdownlint.cmd` on windows

--- lua/lint/linters/markdownlint.lua
+++ lua/lint/linters/markdownlint.lua
@@ -1,6 +1,7 @@
 local efm = '%f:%l:%c %m,%f:%l %m'
+local is_windows = vim.loop.os_uname().version:match('Windows')
 return {
-    cmd = 'markdownlint',
+    cmd = is_windows and 'markdownlint.cmd' or 'markdownlint',
     ignore_exitcode = true,
     stream = 'stderr',
     parser =  require('lint.parser').from_errorformat(efm, {
