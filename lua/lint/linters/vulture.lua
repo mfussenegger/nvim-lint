e454d191ba7cdc83ceebbf4f4fab76cacc3d1f71
manuflya <manuel.floriano@flyability.com>
2021-12-27 18:40:24 +0100

    feat: add vulture linter

---
+++ lua/lint/linters/vulture.lua
@@ -0,0 +1,14 @@
+-- path/to/file:line:col: code message
+local pattern = '([^:]+):(%d+):(.*)'
+local groups = { 'file', 'lnum', 'message' }
+
+return {
+  cmd = 'vulture',
+  stdin = false,
+  args = {},
+  ignore_exitcode = true,
+  parser = require('lint.parser').from_pattern(pattern, groups, nil, {
+    ['source'] = 'vulture',
+    ['severity'] = vim.diagnostic.severity.WARN,
+  }),
+}
