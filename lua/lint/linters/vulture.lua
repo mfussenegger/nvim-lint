fd2c5a3dc1993c37f6a61f7274640eec51e1eb76
manuflya <manuel.floriano@flyability.com>
2022-01-04 23:15:58 +0100

    fix: Add the git root dir to input argument if exists, current dir otherwise

--- lua/lint/linters/vulture.lua
+++ lua/lint/linters/vulture.lua
@@ -1,12 +1,22 @@
 -- path/to/file:line: message
-local pattern = '([^:]+):(%d+):(.*)'
+local pattern = '([^:]+):(%d+): (.*)'
 local groups = { 'file', 'lnum', 'message' }
 
 return {
   cmd = 'vulture',
   stdin = false,
-  args = {},
+  args = {"--exclude='/**/docs/*.py,/**/build/*.py'", function()
+    local output = vim.fn.system("git rev-parse --show-toplevel"):sub(1, -2)
+    if output:match("^([%w]+)") == "fatal" then
+        -- Return the current dir
+        return vim.fn.getcwd()
+    else
+        -- Return the path to git root directory
+        return output
+    end
+  end},
   ignore_exitcode = true,
+  append_fname = false,
   parser = require('lint.parser').from_pattern(pattern, groups, nil, {
     ['source'] = 'vulture',
     ['severity'] = vim.diagnostic.severity.WARN,
