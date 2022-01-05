3a5a20d682e5ebb44c6479a78175c1f4a0f9a26c
mflova <67102627+mflova@users.noreply.github.com>
2022-01-05 18:36:55 +0100

    Improve vulture scope (#154)
    Co-authored-by: manuflya <manuel.floriano@flyability.com>
    Co-authored-by: Mathias Fußenegger <mfussenegger@users.noreply.github.com>

--- lua/lint/linters/vulture.lua
+++ lua/lint/linters/vulture.lua
@@ -1,12 +1,20 @@
 -- path/to/file:line: message
-local pattern = '([^:]+):(%d+):(.*)'
+local pattern = '([^:]+):(%d+): (.*)'
 local groups = { 'file', 'lnum', 'message' }
 
 return {
   cmd = 'vulture',
   stdin = false,
-  args = {},
+  args = {"--exclude='/**/docs/*.py,/**/build/*.py'", function()
+    local git_root_or_err = vim.fn.system("git rev-parse --show-toplevel"):sub(1, -2)
+    if vim.startswith(git_root_or_err, 'fatal') then
+        return vim.fn.getcwd()
+    else
+        return git_root_or_err
+    end
+  end},
   ignore_exitcode = true,
+  append_fname = false,
   parser = require('lint.parser').from_pattern(pattern, groups, nil, {
     ['source'] = 'vulture',
     ['severity'] = vim.diagnostic.severity.WARN,
