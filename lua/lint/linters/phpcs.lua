39d9cf321d49df666e1998b1ed599c74d1a3d37f
Austin Burdine <acburdine@gmail.com>
2022-01-16 12:51:12 -0500

    Add phpcs linter

---
+++ lua/lint/linters/phpcs.lua
@@ -0,0 +1,38 @@
+local severities = {
+  ERROR = vim.diagnostic.severity.ERROR,
+  WARNING = vim.diagnostic.severity.WARN,
+}
+
+return {
+  cmd = 'phpcs',
+  stdin = true,
+  args = {
+    '-q',
+    '--report=json',
+    '-', -- need `-` at the end for stdin support
+  },
+  ignore_exitcode = true,
+  parser = function(output, _)
+    if vim.trim(output) == '' then
+      return {}
+    end
+
+    local decoded = vim.fn.json_decode(output)
+    local diagnostics = {}
+    local messages = decoded['files']['STDIN']['messages']
+
+    for _, msg in ipairs(messages or {}) do
+      table.insert(diagnostics, {
+        lnum = msg.line - 1,
+        end_lnum = msg.line - 1,
+        col = msg.column - 1,
+        end_col = msg.column - 1,
+        message = msg.message,
+        source = 'phpcs',
+        severity = assert(severities[msg.type], 'missing mapping for severity ' .. msg.type),
+      })
+    end
+
+    return diagnostics
+  end
+}
