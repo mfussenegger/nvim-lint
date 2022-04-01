462f5b3cc60c94e25d0b2bf2bbaa43f4cf904678
Stephen Bolton <stephen@bitsonthemind.com>
2022-04-01 03:02:40 -0600

    Add credo linter (#196)
    This is the standard linter used in the elixir community.

---
+++ lua/lint/linters/credo.lua
@@ -0,0 +1,12 @@
+local errorfmt = '[%t] %. %f:%l:%c %m, [%t] %. %f:%l %m'
+
+return {
+  cmd = 'mix',
+  stdin = true,
+  args = { 'credo', 'list', '--format=oneline', '--read-from-stdin', '--strict'},
+  stream = 'stdout',
+  ignore_exitcode = true, -- credo only returns 0 if there are no errors
+  parser = require('lint.parser').from_errorformat(errorfmt)
+}
+
+
