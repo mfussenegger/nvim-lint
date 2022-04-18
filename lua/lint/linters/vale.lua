e7732ad44efc358ceb54b7139ffb32aa6867ea18
Mathias Fussenegger <f.mathias@zignar.net>
2022-04-18 11:50:22 +0200

    vale: Handle byteindex conversion errors gracefully

--- lua/lint/linters/vale.lua
+++ lua/lint/linters/vale.lua
@@ -29,8 +29,18 @@ return {
     local items = decoded['stdin' .. get_cur_file_extension(bufnr)]
     for _, item in pairs(items or {}) do
       local curline = unpack(vim.api.nvim_buf_get_lines(bufnr, item.Line-1, item.Line, false))
-      local column = vim.str_byteindex(curline, item.Span[1])
-      local end_column = vim.str_byteindex(curline, item.Span[2])
+      -- Vale can report diagnostics not specific to a line/word but about the overall document in the first line.
+      -- An example are the readability scores.
+      -- The first line could be empty in which case the conversion here would fail
+      local ok, column = pcall(vim.str_byteindex, curline, item.Span[1])
+      if not ok then
+        column = 1
+      end
+      local end_column
+      ok, end_column = vim.str_byteindex(curline, item.Span[2])
+      if not ok then
+        end_column = #curline
+      end
       table.insert(diagnostics, {
         lnum = item.Line - 1,
         end_lnum = item.Line - 1,
