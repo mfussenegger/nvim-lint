3448f7d83e01e5d7b359844d193b4804b42aa844
Satoshi Matsubara <stackline@users.noreply.github.com>
2022-06-18 02:11:39 +0900

    Add rubocop linter (#224)

---
+++ lua/lint/linters/rubocop.lua
@@ -0,0 +1,35 @@
+local severity_map = {
+  ['fatal'] = vim.diagnostic.severity.ERROR,
+  ['error'] = vim.diagnostic.severity.ERROR,
+  ['warning'] = vim.diagnostic.severity.WARN,
+  ['convention'] = vim.diagnostic.severity.HINT,
+  ['refactor'] = vim.diagnostic.severity.INFO,
+  ['info'] = vim.diagnostic.severity.INFO,
+}
+
+return {
+  cmd = 'rubocop',
+  stdin = false,
+  args = {'--format', 'json', '--force-exclusion'},
+  ignore_exitcode = true,
+  parser = function(output)
+    local diagnostics = {}
+    local decoded = vim.json.decode(output)
+    local offences = decoded.files[1].offenses
+
+    for _, off in pairs(offences) do
+      table.insert(diagnostics, {
+        source = 'rubocop',
+        lnum = off.location.start_line - 1,
+        col = off.location.start_column - 1,
+        end_lnum = off.location.last_line - 1,
+        end_col = off.location.last_column,
+        severity = severity_map[off.severity],
+        message = off.message,
+        code = off.cop_name
+      })
+    end
+
+    return diagnostics
+  end,
+}
