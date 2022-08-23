e740517ff250fcaa6a1a69f6f079dd3af4e50ed0
Felipe Lema <1232306+FelipeLema@users.noreply.github.com>
2022-08-23 14:41:38 -0400

    Add sqlfluff (#241)

---
+++ lua/lint/linters/sqlfluff.lua
@@ -0,0 +1,29 @@
+return {
+  cmd = "sqlfluff",
+  args = {
+    "lint", "--format=json",
+    -- note: users will have to replace the --dialect argument accordingly
+    "--dialect=postgres", "-",
+  },
+  ignore_exitcode = true,
+  stdin = true,
+  parser = function(output, _)
+    local per_filepath = #output > 0 and vim.json.decode(output) or {}
+    local diagnostics = {}
+    for _, i_filepath in ipairs(per_filepath) do
+      if i_filepath.filepath == "stdin" then -- only process stdin
+        for _, violation in ipairs(i_filepath.violations) do
+          table.insert(diagnostics, {
+            source = 'sqlfluff',
+            lnum = violation.line_no - 1,
+            col = violation.line_pos - 1,
+            severity = vim.diagnostic.severity.ERROR,
+            message = violation.description,
+            user_data = {lsp = {code = violation.code}},
+          })
+        end
+      end
+    end
+    return diagnostics
+  end,
+}
