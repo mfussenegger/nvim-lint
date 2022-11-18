94058e99cfbbc85ae9a1f89639b89a433b925811
Daniel Rotter <daniel.rotter@gmail.com>
2022-11-18 16:15:06 +0100

    Fix pipeline

--- lua/lint/linters/psalm.lua
+++ lua/lint/linters/psalm.lua
@@ -1,29 +1,29 @@
 return {
-	cmd = 'psalm',
-	args = {
-		'--output-format=json',
-		'--no-progress',
-	},
-	parser = function(output)
-		if output == nil then
-			return {}
-		end
+  cmd = 'psalm',
+  args = {
+    '--output-format=json',
+    '--no-progress',
+  },
+  parser = function(output)
+    if output == nil then
+      return {}
+    end
 
-		local messages = vim.json.decode(output)
-		local diagnostics = {}
+    local messages = vim.json.decode(output)
+    local diagnostics = {}
 
-		for _, message in ipairs(messages or {}) do
-			table.insert(diagnostics, {
-				lnum = message.line_from - 1,
-				end_lnum = message.line_to - 1,
-				col = message.column_from - 1,
-				end_col = message.column_to - 1,
-				message = message.message,
-				source = 'psalm',
-				severity = message.severity,
-			})
-		end
+    for _, message in ipairs(messages or {}) do
+      table.insert(diagnostics, {
+        lnum = message.line_from - 1,
+        end_lnum = message.line_to - 1,
+        col = message.column_from - 1,
+        end_col = message.column_to - 1,
+        message = message.message,
+        source = 'psalm',
+        severity = message.severity,
+      })
+    end
 
-		return diagnostics;
-	end,
+    return diagnostics;
+  end,
 }
