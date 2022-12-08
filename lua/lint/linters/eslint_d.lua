5834fb5177fa687ca94cc7440a2286de02f51d48
Sanchayan Maity <sanchayan@sanchayanmaity.net>
2022-12-08 15:45:53 +0530

    Add eslint_d

---
+++ lua/lint/linters/eslint_d.lua
@@ -0,0 +1,23 @@
+local pattern = [[%s*(%d+):(%d+)%s+(%w+)%s+(.+%S)%s+(%S+)]]
+local groups = { 'lnum', 'col', 'severity', 'message', 'code' }
+local severity_map = {
+  ['error'] = vim.diagnostic.severity.ERROR,
+  ['warn'] = vim.diagnostic.severity.WARN,
+  ['warning'] = vim.diagnostic.severity.WARN,
+}
+
+return {
+  cmd = function()
+    local local_eslintd = vim.fn.fnamemodify('./node_modules/.bin/eslint_d', ':p')
+    local stat = vim.loop.fs_stat(local_eslintd)
+    if stat then
+      return local_eslintd
+    end
+    return 'eslint_d'
+  end,
+  args = {},
+  stdin = false,
+  stream = 'stdout',
+  ignore_exitcode = true,
+  parser = require('lint.parser').from_pattern(pattern, groups, severity_map, { ['source'] = 'eslint_d' }),
+}
