9c9490f63428f667fd3fcc7025947323653e17cc
Eleanor Bartle <eleanor@eleanor-nb.com>
2023-01-05 21:03:34 +1100

    Add option to use cppcheck with C (#283)

--- lua/lint/linters/cppcheck.lua
+++ lua/lint/linters/cppcheck.lua
@@ -1,25 +1,31 @@
 -- cppcheck <= 1.84 doesn't support {column} so the start_col group is ambiguous
 local pattern = [[([^:]*):(%d*):([^:]*): %[([^%]\]*)%] ([^:]*): (.*)]]
-local groups = { 'file', 'lnum', 'col', 'code', 'severity', 'message' }
+local groups = { "file", "lnum", "col", "code", "severity", "message" }
 local severity_map = {
-  ['error'] = vim.diagnostic.severity.ERROR,
-  ['warning'] = vim.diagnostic.severity.WARN,
-  ['performance'] = vim.diagnostic.severity.WARN,
-  ['style'] = vim.diagnostic.severity.INFO,
-  ['information'] = vim.diagnostic.severity.INFO,
+  ["error"] = vim.diagnostic.severity.ERROR,
+  ["warning"] = vim.diagnostic.severity.WARN,
+  ["performance"] = vim.diagnostic.severity.WARN,
+  ["style"] = vim.diagnostic.severity.INFO,
+  ["information"] = vim.diagnostic.severity.INFO,
 }
 
 return {
-  cmd = 'cppcheck',
+  cmd = "cppcheck",
   stdin = false,
   args = {
-    '--enable=warning,style,performance,information',
-    '--language=c++',
-    '--inline-suppr',
-    '--quiet',
-    '--cppcheck-build-dir=build',
-    '--template={file}:{line}:{column}: [{id}] {severity}: {message}',
+    "--enable=warning,style,performance,information",
+    function()
+      if vim.bo.filetype == "cpp" then
+        return "--language=c++"
+      else
+        return "--language=c"
+      end
+    end,
+    "--inline-suppr",
+    "--quiet",
+    "--cppcheck-build-dir=build",
+    "--template={file}:{line}:{column}: [{id}] {severity}: {message}",
   },
-  stream = 'stderr',
-  parser = require('lint.parser').from_pattern(pattern, groups, severity_map, { ['source'] = 'cppcheck' }),
+  stream = "stderr",
+  parser = require("lint.parser").from_pattern(pattern, groups, severity_map, { ["source"] = "cppcheck" }),
 }
