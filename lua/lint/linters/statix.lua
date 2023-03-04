fb308f94e41adc9788b23c6ef6326fe47870ce32
Daniel Kempkens <daniel+git@kempkens.io>
2023-03-04 01:18:16 +0100

    fix: statix returns a non-zero exitcode if there are any errors

--- lua/lint/linters/statix.lua
+++ lua/lint/linters/statix.lua
@@ -3,6 +3,7 @@ return {
   stdin = true,
   args = {'check', '-o', 'errfmt', '--stdin'},
   stream = 'stdout',
+  ignore_exitcode = true, -- statix only returns 0 if there are no errors
   parser = require('lint.parser').from_errorformat('%f>%l:%c:%t:%n:%m', {
     source = 'statix'
   })
