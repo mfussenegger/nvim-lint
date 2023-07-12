87fb585cc9a378e993c7d0a3b183c454eeeecc2e
Geraint Hywel <g.s.j.hywel@swansea.ac.uk>
2023-07-12 17:25:21 +0100

    feat: added linter for PHPMD

---
+++ lua/lint/linters/phpmd.lua
@@ -0,0 +1,52 @@
+local severities = {}
+severities[1] = vim.diagnostic.severity.ERROR
+severities[2] = vim.diagnostic.severity.WARN
+severities[3] = vim.diagnostic.severity.INFO
+severities[4] = vim.diagnostic.severity.HINT
+severities[5] = vim.diagnostic.severity.HINT
+
+return {
+  cmd = 'vendor/bin/phpmd',
+  stdin = true,
+  args = {
+    '-',
+    'json',
+    'cleancode,codesize,controversial,design,naming,unusedcode',
+  },
+  stream = 'stdout',
+  ignore_exitcode = true,
+  parser = function(output, _)
+
+    if vim.trim(output) == '' or output == nil then
+      return {}
+    end
+
+    if not vim.startswith(output, '{') then
+      vim.notify(output)
+      return {}
+    end
+
+
+    local decoded = vim.json.decode(output)
+    local diagnostics = {}
+    local messages = {}
+
+    if decoded['files'] and decoded['files'][1] and decoded['files'][1]['violations'] then
+      messages = decoded['files'][1]['violations']
+    end
+
+    for _, msg in ipairs(messages or {}) do
+      table.insert(diagnostics, {
+        lnum = msg.beginLine - 1,
+        end_lnum = msg.endLine - 1,
+        col = 0,
+        end_col = 0,
+        message = msg.description,
+        source = 'phpmd',
+        severity = assert(severities[msg.priority], 'missing mapping for severity ' .. msg.priority),
+      })
+    end
+
+    return diagnostics
+  end
+}
