ce8a4af7b850deb9fca9a03227cc369adb4a719c
Dan Sully <dan+github@sully.org>
2023-09-17 04:54:28 -0700

    Update lua/lint/linters/typos.lua
    Co-authored-by: Mathias Fußenegger <mfussenegger@users.noreply.github.com>

--- lua/lint/linters/typos.lua
+++ lua/lint/linters/typos.lua
@@ -18,40 +18,16 @@ return {
       return {}
     end
 
-    -- From: https://github.com/turboladen/dotfiles/blob/master/.config/nvim/lua/plugins/nvim-lint.lua#L90
-    -- typos-cli, when used with `--format json` outputs errors that look like this:
-    --
+    -- Each line contains an entry of JSON. E.g.:
     --   {"type":"binary_file","path":"./lua/init_rs.so"}
     --   {"type":"typo","path":"./lua/plugins/23_package-info-nvim.lua","line_num":15,"byte_offset":37,"typo":"nd","corrections":["and"]}
     --   {"type":"binary_file","path":"./spell/en.utf-8.add.spl"}
-
-    -- Takes an array of suggested corrections (strings) and builds an output string.len
-    -- If the array is only one element, the output will be that string; if it's more than one,
-    -- the elements are joined with "or" (ex. `{"meow", "taco"}` -> `"meow or taco"`). explicitly
-    --
-    ---@param all_corrections table
-    ---@return string
-    local function build_corrections(all_corrections)
-      local corrections = ""
-
-      if #all_corrections == 1 then
-        return all_corrections[1]
-      end
-
-      for _, correction in ipairs(all_corrections) do
-        corrections = corrections .. " or " .. correction
-      end
-
-      return corrections
-    end
-
-    -- Split the output string on newlines, where each line contains an entry of JSON.
     for json in string.gmatch(output, "[%S]+") do
       local item = vim.json.decode(json)
 
       if item ~= nil then
         local line_num = item.line_num - 1
-        local corrections = build_corrections(item.corrections)
+        local corrections = table.concat(item.corrections, " or ")
 
         table.insert(diagnostics, {
           lnum = line_num,
