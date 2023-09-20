2e1564d1426427d85d6ec0626f18188b11b08e34
Lopy <70210066+lopi-py@users.noreply.github.com>
2023-09-19 20:59:02 -0500

    fix: selene linter

--- lua/lint/linters/selene.lua
+++ lua/lint/linters/selene.lua
@@ -5,8 +5,8 @@ local severities = {
 
 return {
     cmd = "selene",
-    stdin = false,
-    args = { "--display-style", "json" },
+    stdin = true,
+    args = { "--display-style", "json", "-" },
     stream = "stdout",
     ignore_exitcode = true,
     parser = function(output)
@@ -22,12 +22,6 @@ return {
             table.insert(labels, decoded.primary_label)
 
             for _, label in ipairs(labels) do
-                local start_offset = label.span.start
-                local start_line = vim.fn.byte2line(start_offset)
-
-                local end_offset = label.span["end"]
-                local end_line = vim.fn.byte2line(end_offset)
-
                 local message = decoded.message
                 if label.message ~= "" then
                     message = message .. ". " .. label.message
@@ -45,10 +39,10 @@ return {
                         severities[decoded.severity],
                         "missing mapping for severity " .. decoded.severity
                     ),
-                    lnum = start_line - 1,
-                    col = start_offset - vim.fn.line2byte(start_line) - 1,
-                    end_lnum = end_line - 1,
-                    end_col = end_offset - vim.fn.line2byte(end_line) - 1,
+                    lnum = label.span.start_line,
+                    col = label.span.start_column,
+                    end_lnum = label.span.end_line,
+                    end_col = label.span.end_column,
                     message = message,
                 })
             end
