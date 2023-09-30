5b2eb55a515a9202647fde0fd136e284bcd66472
Emmanuel Touzery <etouzery@gmail.com>
2023-09-30 15:43:22 +0200

    eslint: use stdin instead of filename

--- lua/lint/linters/eslint.lua
+++ lua/lint/linters/eslint.lua
@@ -15,8 +15,12 @@ return require('lint.util').inject_cmd_exe({
     end
     return 'eslint'
   end,
-  args = {},
-  stdin = false,
+  args = {
+    '--stdin',
+    '--stdin-filename',
+    function() return vim.api.nvim_buf_get_name(0) end,
+  },
+  stdin = true,
   stream = 'stdout',
   ignore_exitcode = true,
   parser = require('lint.parser').from_pattern(pattern, groups, severity_map, { ['source'] = 'eslint' }),
