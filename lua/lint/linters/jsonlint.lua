aa95105d8b0c14920599e094407b0a4955a843b5
Jędrzej Orbik <Jendker@users.noreply.github.com>
2023-10-11 18:40:03 +0200

    Use stdin with jsonlint (#401)

--- lua/lint/linters/jsonlint.lua
+++ lua/lint/linters/jsonlint.lua
@@ -1,10 +1,15 @@
+local pattern = "line (%d+), col (%d+), (.*)"
+local groups = { "lnum", "col", "message" }
+local severities = nil -- none provided
+
 return {
   cmd = 'jsonlint',
   stream = 'stderr',
   args = { '--compact' },
+  stdin = true,
   ignore_exitcode = true,
-  parser = require('lint.parser').from_errorformat("%f:\\ line\\ %l\\,\\ col\\ %c\\, %m", {
+  parser = require('lint.parser').from_pattern(pattern, groups, severities, {
     source = 'jsonlint',
     severity = vim.diagnostic.severity.ERROR,
-  })
+  }),
 }
