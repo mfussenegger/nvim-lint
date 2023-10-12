fd646f49e9a2c0836035e1ce7a3ec25ce148bae8
Christoffer Klang <christoffer.klang@gmail.com>
2023-10-12 10:09:31 +0200

    languagetool: Fix parsing error when output contains 'Err:...' (#396)

--- lua/lint/linters/languagetool.lua
+++ lua/lint/linters/languagetool.lua
@@ -1,9 +1,27 @@
+-- LanguageTool might give output like "Err: 'yada yada'\n{ ... json here ... }'
+local function parse_err_json(str)
+    local json_start = str:find('{', 1, true)
+    local err = nil
+    local json = str
+
+    if json_start and json_start > 1 then
+        err = str:sub(1, json_start - 1):gsub("^%s*(.-)%s*$", "%1") -- trim spaces
+        json = str:sub(json_start)
+    end
+
+    return err, json
+end
+
 return {
   cmd = 'languagetool',
   args = {'--autoDetect', '--json'},
   stream = 'stdout',
   parser = function(output, bufnr)
-    local decoded = vim.json.decode(output)
+    local err, json = parse_err_json(output)
+    if err then
+      vim.notify_once(err, vim.log.levels.INFO)
+    end
+    local decoded = vim.json.decode(json)
     local diagnostics = {}
     local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, true)
     local content = table.concat(lines, '\n')
