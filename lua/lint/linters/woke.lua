43abe6e11f89230927cb1facbd8498efe7c428ca
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-10-17 19:37:55 +0200

    fix: parse json instead of errorformat for `woke`

--- lua/lint/linters/woke.lua
+++ lua/lint/linters/woke.lua
@@ -1,9 +1,43 @@
+local severities = {
+  warn = vim.diagnostic.severity.WARN,
+  warning = vim.diagnostic.severity.WARN,
+  error = vim.diagnostic.severity.ERROR,
+  info = vim.diagnostic.severity.INFO,
+  hint = vim.diagnostic.severity.HINT,
+}
+
 return {
   cmd = "woke",
-  args = { "--stdin", "--output=simple" },
+  args = { "--stdin", "--output=json" },
   stdin = true,
-  parser = require("lint.parser").from_errorformat(
-    "/dev/stdin:%l:%c: [%tarning] %m,/dev/stdin:%l:%c: [%trror] %m",
-    { source = "woke", severity = vim.diagnostic.severity.INFO }
-  ),
+  parser = function(output, _)
+    if output == "" or vim.trim(output) == "No findings found." then
+      return {}
+    end
+    local decoded = vim.json.decode(output)
+    if decoded == nil then
+      return {}
+    end
+    local diagnostics = {}
+    for _, item in ipairs(decoded.Results) do
+      local msg = item.Rule.Note
+      if not msg or msg == "" then
+        msg = item.Reason
+      end
+      table.insert(diagnostics, {
+        lnum = item.StartPosition.Line - 1,
+        end_lnum = item.EndPosition.Line - 1,
+        col = item.StartPosition.Column,
+        end_col = item.EndPosition.Column,
+        severity = assert(
+          severities[item.Rule.Severity],
+          "Missing mapping for severity " .. item.Rule.Severity
+        ),
+        source = "woke",
+        code = item.Rule.Name,
+        message = msg,
+      })
+    end
+    return diagnostics
+  end,
 }
