22b235f1e4efc3edae3de430fc9e5e45be20405e
Mathias Fussenegger <f.mathias@zignar.net>
2023-10-17 17:17:36 +0200

    Revert "Add end_col to results of eslint/eslintd (#409)"
    This reverts commit 001eb1e3de19c5c14e4d160e583798d16a1fe283.

--- tests/eslint_d_spec.lua
+++ tests/eslint_d_spec.lua
@@ -7,55 +7,48 @@ describe("linter.eslint_d", function()
 
   it("can parse output", function()
     local parser = require("lint.linters.eslint_d").parser
+    local result = parser(
+      [[
+/directory/file.js
+  1:10  error  'testFunc' is defined but never used                                                                                   no-unused-vars
+  4:16  error  This branch can never execute. Its condition is a duplicate or covered by previous conditions in the if-else-if chain  no-dupe-else-if
 
-    local result = parser([[
-{
-  "messages": [
-    {
-      "column": 10,
-      "endColumn": 18,
-      "endLine": 1,
-      "line": 1,
-      "message": "'testFunc' is defined but never used",
-      "ruleId": "no-unused-vars",
-      "severity": 2
-    },
-    {
-      "column": 16,
-      "endColumn": 22,
-      "endLine": 4,
-      "line": 4,
-      "message": "This branch can never execute. Its condition is a duplicate or covered by previous conditions in the if-else-if chain",
-      "ruleId": "no-dupe-else-if",
-      "severity": 2
-    }
-  ]
-}]])
-
+✖ 2 problems (2 errors, 0 warnings)
+    ]],
+      vim.api.nvim_get_current_buf()
+    )
     assert.are.same(2, #result)
-
     local expected_1 = {
       code = "no-unused-vars",
       col = 9,
-      end_col = 17,
+      end_col = 9,
       end_lnum = 0,
       lnum = 0,
       message = "'testFunc' is defined but never used",
       severity = 1,
       source = "eslint_d",
+      user_data = {
+        lsp = {
+          code = "no-unused-vars",
+        },
+      },
     }
     assert.are.same(expected_1, result[1])
 
     local expected_2 = {
       code = "no-dupe-else-if",
       col = 15,
-      end_col = 21,
+      end_col = 15,
       end_lnum = 3,
       lnum = 3,
-      message =
-      "This branch can never execute. Its condition is a duplicate or covered by previous conditions in the if-else-if chain",
+      message = "This branch can never execute. Its condition is a duplicate or covered by previous conditions in the if-else-if chain",
       severity = 1,
       source = "eslint_d",
+      user_data = {
+        lsp = {
+          code = "no-dupe-else-if",
+        },
+      },
     }
     assert.are.same(expected_2, result[2])
   end)
