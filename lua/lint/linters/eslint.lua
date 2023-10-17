749e895ae631d06d8651081fb6e76d3e43bae035
Mathias Fußenegger <mfussenegger@users.noreply.github.com>
2023-10-17 17:33:32 +0200

    Revert broken eslint changes (#419)
    * Revert "Fix parsing regression in eslint(d) (#416)"
    
    This reverts commit 7ac6307eed35f1a15cb4f3692fddd6fc61ba47e6.
    
    * Revert "Add end_col to results of eslint/eslintd (#409)"
    
    This reverts commit 001eb1e3de19c5c14e4d160e583798d16a1fe283.

--- lua/lint/linters/eslint.lua
+++ lua/lint/linters/eslint.lua
@@ -1,22 +1,21 @@
-local severities = {
-  vim.diagnostic.severity.WARN,
-  vim.diagnostic.severity.ERROR,
+local pattern = [[%s*(%d+):(%d+)%s+(%w+)%s+(.+%S)%s+(%S+)]]
+local groups = { 'lnum', 'col', 'severity', 'message', 'code' }
+local severity_map = {
+  ['error'] = vim.diagnostic.severity.ERROR,
+  ['warn'] = vim.diagnostic.severity.WARN,
+  ['warning'] = vim.diagnostic.severity.WARN,
 }
 
 return require('lint.util').inject_cmd_exe({
   cmd = function()
     local local_eslint = vim.fn.fnamemodify('./node_modules/.bin/eslint', ':p')
     local stat = vim.loop.fs_stat(local_eslint)
-
     if stat then
       return local_eslint
     end
-
     return 'eslint'
   end,
   args = {
-    '--format',
-    'json',
     '--stdin',
     '--stdin-filename',
     function() return vim.api.nvim_buf_get_name(0) end,
@@ -24,29 +23,5 @@ return require('lint.util').inject_cmd_exe({
   stdin = true,
   stream = 'stdout',
   ignore_exitcode = true,
-  parser = function(output)
-    local success, decodedData = pcall(
-      vim.json.decode, output,
-      { luanil = { object = true, array = true } }
-    )
-    local messages = decodedData and decodedData[1] and decodedData[1].messages or {}
-
-    local diagnostics = {}
-    if success and #messages > 0 then
-      for _, diagnostic in ipairs(messages or {}) do
-        table.insert(diagnostics, {
-          source = "eslint",
-          lnum = diagnostic.line - 1,
-          col = diagnostic.column - 1,
-          end_lnum = (diagnostic.endLine or diagnostic.line) - 1,
-          end_col = (diagnostic.endColumn or diagnostic.column) - 1,
-          severity = severities[diagnostic.severity],
-          message = diagnostic.message,
-          code = diagnostic.ruleId
-        })
-      end
-    end
-
-    return diagnostics
-  end
+  parser = require('lint.parser').from_pattern(pattern, groups, severity_map, { ['source'] = 'eslint' }),
 })
