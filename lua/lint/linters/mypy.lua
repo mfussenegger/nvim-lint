a4b0511a9e757e78c8c9f9d3f201e0d8ff158b42
landeaux <landeaux@users.noreply.github.com>
2023-10-22 00:20:28 -0700

    mypy: Add end_lnum and end_col (#432)

--- lua/lint/linters/mypy.lua
+++ lua/lint/linters/mypy.lua
@@ -1,6 +1,6 @@
 -- path/to/file:line:col: severity: message
-local pattern = '([^:]+):(%d+):(%d+): (%a+): (.*)'
-local groups = { 'file', 'lnum', 'col', 'severity', 'message' }
+local pattern = '([^:]+):(%d+):(%d+):(%d+):(%d+): (%a+): (.*)'
+local groups = { 'file', 'lnum', 'col', 'end_lnum', 'end_col', 'severity', 'message' }
 local severities = {
   error = vim.diagnostic.severity.ERROR,
   warning = vim.diagnostic.severity.WARN,
@@ -13,11 +13,18 @@ return {
   ignore_exitcode = true,
   args = {
     '--show-column-numbers',
+    '--show-error-end',
     '--hide-error-codes',
     '--hide-error-context',
     '--no-color-output',
     '--no-error-summary',
     '--no-pretty',
   },
-  parser = require('lint.parser').from_pattern(pattern, groups, severities, { ['source'] = 'mypy' }),
+  parser = require('lint.parser').from_pattern(
+    pattern,
+    groups,
+    severities,
+    { ['source'] = 'mypy' },
+    { end_col_offset = 0 }
+  ),
 }
