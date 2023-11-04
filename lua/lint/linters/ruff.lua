2eb651843203e41ff13f555c33a96b54834611e8
Avishay Matayev <avishay@decart.ai>
2023-11-04 16:50:41 +0200

    ruff: Change to json format; Include end_col
    The end column is not reported otherwise. It is required for underlining
    the diagnostic.

--- lua/lint/linters/ruff.lua
+++ lua/lint/linters/ruff.lua
@@ -1,24 +1,40 @@
--- path/to/file:line:col: code message
-local pattern = '[^:]+:(%d+):(%d+): (%w+) (.+)'
-local groups = { 'lnum', 'col', 'code', 'message' }
 local function get_file_name()
   return vim.api.nvim_buf_get_name(0)
 end
 
 return {
-  cmd = 'ruff',
+  cmd = "ruff",
   stdin = true,
   args = {
-    '--force-exclude',
-    '--quiet',
-    '--stdin-filename',
+    "--force-exclude",
+    "--quiet",
+    "--stdin-filename",
     get_file_name,
-    '--no-fix',
-    '-',
+    "--no-fix",
+    "--output-format",
+    "json",
+    "-",
   },
   ignore_exitcode = true,
-  parser = require('lint.parser').from_pattern(pattern, groups, nil, {
-    ['source'] = 'ruff',
-    ['severity'] = vim.diagnostic.severity.WARN,
-  }),
+  stream = "stdout",
+  parser = function(output)
+    local diagnostics = {}
+    local ok, results = pcall(vim.json.decode, output)
+    if not ok then
+      return diagnostics
+    end
+    for _, result in ipairs(results or {}) do
+      local err = {
+        message = result.message,
+        col = result.location.column - 1,
+        end_col = result.end_location.column - 1,
+        lnum = result.location.row - 1,
+        end_lnum = result.end_location.row - 1,
+        code = result.code,
+        severity = vim.diagnostic.severity.WARN,
+      }
+      table.insert(diagnostics, err)
+    end
+    return diagnostics
+  end,
 }
