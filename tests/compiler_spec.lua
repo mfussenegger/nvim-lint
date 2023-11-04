7746f952827dabfb70194518c99c93d5651b8f19
Mathias Fussenegger <f.mathias@zignar.net>
2023-11-04 16:53:51 +0100

    Fix fd leak
    85108b3 introduced a regression, causing fd leaks
    
    Closes https://github.com/mfussenegger/nvim-lint/issues/448

--- tests/compiler_spec.lua
+++ tests/compiler_spec.lua
@@ -1,3 +1,10 @@
+local function get_num_handles()
+  local pid = vim.fn.getpid()
+  local output = vim.fn.system({"lsof", "-p"}, tostring(pid))
+  local lines = vim.split(output, "\n", { plain = true })
+  return #lines
+end
+
 describe('compiler', function()
   it('reads errors from both stdout and stderr', function()
     local a = vim.api
@@ -6,9 +13,13 @@ describe('compiler', function()
     a.nvim_buf_set_option(bufnr, 'errorformat', '%l: %m')
     a.nvim_buf_set_option(bufnr, 'makeprg', 'python tests/both.py')
 
+    local handles = get_num_handles()
     require('lint').try_lint('compiler')
 
     vim.wait(5000, function() return next(vim.diagnostic.get(bufnr)) ~= nil end)
+
+    assert.are.same(handles, get_num_handles(), "shouldn't leak any handles")
+
     local result = vim.diagnostic.get(bufnr)
     for _, d in pairs(result) do
       d.namespace = nil
