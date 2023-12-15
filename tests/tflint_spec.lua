5b42e12f397e9fd2629e6f0ce1c780a12b9e6597
Steve Xuereb <steveazz@outlook.com>
2023-12-15 13:03:33 +0100

    tflint: Fix filename/path matching (#487)
    What
    ---
    Update filename match to check relative path instead of absolute path.
    
    Why
    ---
    [`vim.api.nvim_buf_get_name(bufnr)`](https://neovim.io/doc/user/api.html#nvim_buf_get_name()) gets the full path name,
    but `tflint` only returns the relative path from the root of the project.
    Use [`vim.fn.expand("%")`](https://neovim.io/doc/user/builtin.html#expand())
    returns the current filename that is open in the buffer.
    
    Example:
    ```sh
    $ tflint --format=json | jq
    {
      "issues": [
        {
          "rule": {
            "name": "terraform_typed_variables",
            "severity": "warning",
            "link": "https://github.com/terraform-linters/tflint-ruleset-terraform/blob/v0.5.0/docs/rules/terraform_typed_variables.md"
          },
          "message": "`not_used` variable has no type",
          "range": {
            "filename": "main.tf",
            "start": {
              "line": 1,
              "column": 1
            },
            "end": {
              "line": 1,
              "column": 20
            }
          },
          "callers": []
        },
      ],
      "errors": []
    }
    ```
    
    Signed-off-by: Steve Xuereb <steveazz@outlook.com>

--- tests/tflint_spec.lua
+++ tests/tflint_spec.lua
@@ -3,8 +3,9 @@ describe('linter.tflint', function()
     local parser = require('lint.linters.tflint').parser
     local bufnr = vim.uri_to_bufnr('file:///main.tf')
     local result = parser(
-      [[{"issues":[{"rule":{"name":"terraform_required_providers","severity":"warning","link":"https://github.com/terraform-linters/tflint-ruleset-terraform/blob/v0.2.2/docs/rules/terraform_required_providers.md"},"message":"Missing version constraint for provider \"aws\" in \"required_providers\"","range":{"filename":"/main.tf","start":{"line":19,"column":1},"end":{"line":19,"column":15}},"callers":[]}],"errors":[]}]],
-      bufnr)
+      [[{"issues":[{"rule":{"name":"terraform_required_providers","severity":"warning","link":"https://github.com/terraform-linters/tflint-ruleset-terraform/blob/v0.2.2/docs/rules/terraform_required_providers.md"},"message":"Missing version constraint for provider \"aws\" in \"required_providers\"","range":{"filename":"main.tf","start":{"line":19,"column":1},"end":{"line":19,"column":15}},"callers":[]}],"errors":[]}]],
+      bufnr
+    )
     assert.are.same(1, #result)
     local expected = {
       source = 'tflint',
