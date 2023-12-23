32d7104b04b60493ad66384802e90f1db1fd2802
Sandro Covo <sandro@sandrocovo.ch>
2023-12-23 21:10:55 +0100

    Add linter for pony using the ponyc compiler

---
+++ lua/lint/linters/pony.lua
@@ -0,0 +1,15 @@
+local pattern = [[([^:]*):(%d+):(%d+): ([^[]+)]]
+local groups = { 'file', 'lnum', 'col', 'message'}
+
+return {
+  cmd = "ponyc",
+  stdin = false,
+  append_fname = false, 
+  args = { "--pass=verify" },
+  stream = "stderr",
+  ignore_exitcode = true,
+  parser = require("lint.parser").from_pattern(pattern, groups, nil, {
+      ["source"] = "ponyc",
+      ["severity"] = vim.diagnostic.severity.ERROR,
+    }),
+}
