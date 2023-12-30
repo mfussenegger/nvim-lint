c34d1315e1d29bc80f817e598db587928a2f36b4
Jason Gerber <jason@jasongerber.ch>
2023-12-30 18:57:23 +0100

    Only show diagnostics in current buffer

--- lua/lint/linters/psalm.lua
+++ lua/lint/linters/psalm.lua
@@ -19,21 +19,26 @@ return {
       return {}
     end
 
+    local bufnr = vim.api.nvim_get_current_buf()
+    local filename = vim.api.nvim_buf_get_name(bufnr)
+
     local messages = vim.json.decode(output)
     local diagnostics = {}
 
     for _, message in ipairs(messages or {}) do
-      table.insert(diagnostics, {
-        lnum = message.line_from - 1,
-        end_lnum = message.line_to - 1,
-        col = message.column_from - 1,
-        end_col = message.column_to - 1,
-        message = message.message,
-        source = 'psalm',
-        severity = message.severity,
-      })
+      if message.file_path == filename then
+        table.insert(diagnostics, {
+          lnum = message.line_from - 1,
+          end_lnum = message.line_to - 1,
+          col = message.column_from - 1,
+          end_col = message.column_to - 1,
+          message = message.message,
+          source = 'psalm',
+          severity = message.severity,
+        })
+      end
     end
 
-    return diagnostics;
+    return diagnostics
   end,
 }
