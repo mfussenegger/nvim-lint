e94107b65df6e47715a590afae152dd80005901c
fbuchlak <30214087+fbuchlak@users.noreply.github.com>
2024-02-04 16:28:24 +0100

    formatting

--- lua/lint/linters/checkmake.lua
+++ lua/lint/linters/checkmake.lua
@@ -2,15 +2,15 @@ local pattern = "(%d+):(%w+):(.+)"
 local groups = { "lnum", "code", "message" }
 
 return {
-	cmd = "checkmake",
-	stdin = false,
-	append_fname = true,
-	args = {
-		"--format='{{.LineNumber}}:{{.Rule}}:{{.Violation}}\n'",
-	},
-	ignore_exitcode = true,
-	parser = require("lint.parser").from_pattern(pattern, groups, nil, {
-		["source"] = "checkmake",
-		["severity"] = vim.diagnostic.severity.WARN,
-	}),
+  cmd = "checkmake",
+  stdin = false,
+  append_fname = true,
+  args = {
+    "--format='{{.LineNumber}}:{{.Rule}}:{{.Violation}}\n'",
+  },
+  ignore_exitcode = true,
+  parser = require("lint.parser").from_pattern(pattern, groups, nil, {
+    ["source"] = "checkmake",
+    ["severity"] = vim.diagnostic.severity.WARN,
+  }),
 }
