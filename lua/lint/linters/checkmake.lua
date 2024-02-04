1edccfa58d62d40cbe3c7d2623752a7b99a5da2b
fbuchlak <30214087+fbuchlak@users.noreply.github.com>
2024-02-04 16:20:49 +0100

    feat/add-checkmake

---
+++ lua/lint/linters/checkmake.lua
@@ -0,0 +1,16 @@
+local pattern = "(%d+):(%w+):(.+)"
+local groups = { "lnum", "code", "message" }
+
+return {
+	cmd = "checkmake",
+	stdin = false,
+	append_fname = true,
+	args = {
+		"--format='{{.LineNumber}}:{{.Rule}}:{{.Violation}}\n'",
+	},
+	ignore_exitcode = true,
+	parser = require("lint.parser").from_pattern(pattern, groups, nil, {
+		["source"] = "checkmake",
+		["severity"] = vim.diagnostic.severity.WARN,
+	}),
+}
