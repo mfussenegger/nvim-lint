ece6933549a76eaa91e48eaae569bf047c246ee0
Zhou Fang <fang2hou@outlook.com>
2024-02-23 23:59:10 +0900

    feat: add markuplint buildin config

---
+++ lua/lint/linters/markuplint.lua
@@ -0,0 +1,43 @@
+local binary_name = "markuplint"
+
+return {
+  cmd = function()
+    local local_binary = vim.fn.fnamemodify("./node_modules/.bin/" .. binary_name, ":p")
+    return vim.loop.fs_stat(local_binary) and local_binary or binary_name
+  end,
+  args = { "--format", "JSON" },
+  stdin = false,
+  stream = "stdout",
+  ignore_exitcode = true,
+  parser = function(output, bufnr)
+    if vim.trim(output) == "" then
+      return {}
+    end
+
+    local decode_opts = { luanil = { object = true, array = true } }
+    local ok, data = pcall(vim.json.decode, output, decode_opts)
+    if not ok then
+      return {
+        {
+          bufnr = bufnr,
+          lnum = 0,
+          col = 0,
+          message = "Could not parse markuplint output due to: " .. data .. "\noutput: " .. output,
+        },
+      }
+    end
+
+    local diagnostics = {}
+    for _, result in ipairs(data or {}) do
+      table.insert(diagnostics, {
+        lnum = result.line and result.line - 1 or 0,
+        col = result.col and result.col - 1 or 0,
+        message = result.message,
+        code = result.ruleId,
+        severity = result.severity,
+        source = "markuplint",
+      })
+    end
+    return diagnostics
+  end,
+}
