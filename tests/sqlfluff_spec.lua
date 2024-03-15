1e618c75c7e6e3d19510b33d4b626d47b8e3b978
VIM9BOT <un.9bot@gmail.com>
2024-03-15 15:59:53 +0300

    sqlfluff_upgrade - Upgrading to 3.x : 2
    https://docs.sqlfluff.com/en/stable/releasenotes.html#upgrading-to-3-x

--- tests/sqlfluff_spec.lua
+++ tests/sqlfluff_spec.lua
@@ -31,6 +31,38 @@ describe('linter.sqlfluff', function()
     assert.are.same(expected[2], result[2])
 
   end)
+  it('multi-line output from sqlfluff: old version', function()
+    local parser = require('lint.linters.sqlfluff').parser
+    local bufnr = vim.uri_to_bufnr('file:///non-existent.sql')
+    -- actual output I got from running sqlfluff
+    local result = parser([[
+[{"filepath": "stdin", "violations": [{"line_no": 68, "line_pos": 1, "code": "L003", "description": "Expected 1 indentation, found 0 [compared to line 52]"}, {"line_no": 68, "line_pos": 1, "code": "L013", "description": "Column expression without alias. Use explicit `AS` clause."}]}]
+
+]], bufnr)
+    assert.are.same(2, #result)
+
+    local expected = {}
+    expected[1] = {
+      source = 'sqlfluff',
+      message = 'Expected 1 indentation, found 0 [compared to line 52]',
+      lnum = 67, -- mind the line indexing
+      col = 0, -- mind the column indexing
+      severity = vim.diagnostic.severity.ERROR,
+      user_data = {lsp = {code = 'L003'}},
+    }
+    assert.are.same(expected[1], result[1])
+
+    expected[2] = {
+      source = 'sqlfluff',
+      message = 'Column expression without alias. Use explicit `AS` clause.',
+      lnum = 67,
+      col = 0,
+      severity = vim.diagnostic.severity.ERROR,
+      user_data = {lsp = {code = 'L013'}},
+    }
+    assert.are.same(expected[2], result[2])
+
+  end)
   it("bad CLI args end in non-json output", function()
     local parser = require('lint.linters.sqlfluff').parser
     local bufnr = vim.uri_to_bufnr('file:///non-existent.sql')
@@ -41,7 +73,7 @@ Error: Unknown dialect 'postgresql'
 ]], bufnr)
 
     -- not breaking should be enough, the parsing error is reported as "problem in first line-col"
-    assert(not status)
+    assert(status)
 
   end)
 end)
