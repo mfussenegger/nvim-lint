4055dc856d5ac8f6b85748006fd8fa6457e086e8
Markus Nyman <markus@nyman.dev>
2024-04-09 19:36:42 +0300

    ansible-lint: add --nocolor argument (#566)

--- lua/lint/linters/ansible_lint.lua
+++ lua/lint/linters/ansible_lint.lua
@@ -1,6 +1,6 @@
 return {
   cmd = 'ansible-lint',
-  args = { '-p' },
+  args = { '-p', '--nocolor' },
   ignore_exitcode = true,
   parser = require('lint.parser').from_errorformat('%f:%l: %m', {
     source = 'ansible-lint',
