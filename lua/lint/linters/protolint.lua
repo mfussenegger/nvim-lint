2b235150e746fd9d030041126b1649144e57cede
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-06-01 19:18:15 +0200

    feat: add protolint

---
+++ lua/lint/linters/protolint.lua
@@ -0,0 +1,30 @@
+return {
+  cmd = "protolint",
+  stdin = false,
+  append_fname = true,
+  args = { "lint", "--reporter=json" },
+  stream = "stderr",
+  ignore_exitcode = true,
+  env = nil,
+  parser = function(output)
+    if output == "" then
+      return {}
+    end
+    local json_output = vim.json.decode(output)
+    local diagnostics = {}
+    if json_output.lints == nil then
+      return diagnostics
+    end
+    for _, item in ipairs(json_output.lints) do
+      table.insert(diagnostics, {
+        lnum = item.line - 1,
+        col = item.column - 1,
+        message = item.message,
+        file = item.filename,
+        code = item.rule,
+        severity = vim.diagnostic.severity.WARN,
+      })
+    end
+    return diagnostics
+  end,
+}
