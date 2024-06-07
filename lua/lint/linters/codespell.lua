cf6ffafe158441b8c5e2051ab0fe5b1983527ba7
Jędrzej Orbik <Jendker@users.noreply.github.com>
2024-06-07 04:26:53 -0500

    codespell: use stdin (#596)

--- lua/lint/linters/codespell.lua
+++ lua/lint/linters/codespell.lua
@@ -1,11 +1,15 @@
+-- stdout output in the form "63: resourcs ==> resources, resource"
+local pattern = "(%d+): (.*)"
+local groups = { "lnum", "message" }
+local severities = nil -- none provided
 
 return {
   cmd = 'codespell',
-  stdin = false,
+  args = { '--stdin-single-line', "-" },
+  stdin = true,
   ignore_exitcode = true,
-  parser = require('lint.parser').from_errorformat(
-    '%f:%l:%m',
-    { severity = vim.diagnostic.severity.INFO,
-      source = 'codespell'}
-  )
+  parser = require('lint.parser').from_pattern(pattern, groups, severities, {
+    source = 'codespell',
+    severity = vim.diagnostic.severity.INFO,
+  }),
 }
