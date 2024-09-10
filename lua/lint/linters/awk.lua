d5beb49a6557015e724afdf3b7a591e143b6221c
Fredrik Foss-Indrehus <fredrikfoss@fr.urbanpets.no>
2024-09-10 21:51:45 +0200

    Add awk linter

---
+++ lua/lint/linters/awk.lua
@@ -0,0 +1,14 @@
+local pattern = ".-:(%d+):.-[%s%S]*%^%s*(%S.*)"
+local groups = { "lnum", "message" }
+
+return {
+  cmd = "awk",
+  stdin = true,
+  args = { "-f-", "-L" },
+  stream = "stderr",
+  ignore_exitcode = true,
+  parser = require("lint.parser").from_pattern(pattern, groups, nil, {
+    ["source"] = "awk",
+    ["severity"] = vim.diagnostic.severity.ERROR,
+  }),
+}
