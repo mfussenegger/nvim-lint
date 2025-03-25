bd418278840fc46250c7a5dcd4a1a7787b5d8f95
Jonny Loughlin <jonny@jloughlin.dev>
2025-03-25 07:22:23 -0400

    fix: update golangci-lint configuration for v2

--- lua/lint/linters/golangcilint.lua
+++ lua/lint/linters/golangcilint.lua
@@ -10,12 +10,9 @@ return {
   append_fname = false,
   args = {
     'run',
-    '--out-format',
-    'json',
+    '--output.json.path=stdout',
     '--issues-exit-code=0',
     '--show-stats=false',
-    '--print-issued-lines=false',
-    '--print-linter-name=false',
     function()
       return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
     end
