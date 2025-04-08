31cf50566c0b3d962765c70182f11d67fd36a6dc
Emmanuel Touzery <etouzery@gmail.com>
2025-04-08 21:48:30 +0200

    credo: use stdin to communicate with the linter

--- lua/lint/linters/credo.lua
+++ lua/lint/linters/credo.lua
@@ -1,5 +1,5 @@
-local pattern = "%[%a%]%s+(.+)%s+([^:]+):(%d+):?(%d*)%s+(.*)"
-local groups = { "severity", "file", "lnum", "col", "message" }
+local pattern = "%[%a%]%s+(.+)%s+[^:]+:(%d+):?(%d*)%s+(.*)"
+local groups = { "severity", "lnum", "col", "message" }
 local severity = {
   ["↑"] = vim.diagnostic.severity.ERROR,
   ["↗"] = vim.diagnostic.severity.WARN,
@@ -10,8 +10,8 @@ local severity = {
 
 return {
   cmd = "mix",
-  stdin = false,
-  args = { "credo", "list", "--format=oneline", "--strict" },
+  stdin = true,
+  args = { "credo", "--read-from-stdin", "list", "--format=oneline", "--strict" },
   stream = "stdout",
   ignore_exitcode = true, -- credo only returns 0 if there are no errors
   parser = require("lint.parser").from_pattern(pattern, groups, severity, { ["source"] = "credo" }),
