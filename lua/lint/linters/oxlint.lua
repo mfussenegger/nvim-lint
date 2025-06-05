8d628d0eca9b9cdc6e9672a274fefc6b9986ee4b
mpixton <71086131+mpixton@users.noreply.github.com>
2025-06-05 08:26:01 +0000

    oxlint: use severity from linter output instead of defaulting to warning (#788)

--- lua/lint/linters/oxlint.lua
+++ lua/lint/linters/oxlint.lua
@@ -1,11 +1,16 @@
+local pattern = "(.*):(%d*):(%d*): (.*) %[([EW][arnogi]*)%/([a-z%/%-%(%)]*)%]"
+local severities = {
+  ["Error"] = vim.diagnostic.severity.ERROR,
+  ["Warning"] = vim.diagnostic.severity.WARN,
+}
+local groups = { "file", "lnum", "col", "message", "severity", "code" }
+local defaults = { ["source"] = "oxlint" }
+
 return {
   cmd = "oxlint",
   stdin = false,
   args = { "--format", "unix" },
   stream = "stdout",
   ignore_exitcode = true,
-  parser = require("lint.parser").from_errorformat("%f:%l:%c: %m", {
-    source = "oxlint",
-    severity = vim.diagnostic.severity.WARN,
-  }),
+  parser = require("lint.parser").from_pattern(pattern, groups, severities, defaults, {})
 }
