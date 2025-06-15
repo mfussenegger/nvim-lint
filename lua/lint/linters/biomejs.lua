9b39b57af0ae483263f08378d9fbd68a4175cd06
aMOPel <momoPFL@gmail.com>
2025-06-15 17:15:34 +0200

    feat: improve biomejs parser

--- lua/lint/linters/biomejs.lua
+++ lua/lint/linters/biomejs.lua
@@ -1,83 +1,47 @@
--- Sample biome lint output:
+-- Sample biome lint --reporter=github output:
 --
--- index.js:6:13 lint/suspicious/noDoubleEquals  FIXABLE  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
---
---  ✖ Use === instead of ==
---
---    4 │   count += 1;
---    5 │   var f = function (opt) {
---  > 6 │     if (opt == true) {
---      │             ^^
---    7 │       return "true";
---    8 │     }
---
---  ℹ == is only allowed when comparing against null
---
---    4 │   count += 1;
---    5 │   var f = function (opt) {
---  > 6 │     if (opt == true) {
---      │             ^^
---    7 │       return "true";
---    8 │     }
---
---  ℹ Using === may be unsafe if you are relying on type coercion
---
---  ℹ Suggested fix: Use ===
---
---    6 │ ····if·(opt·===·true)·{
---      │
+-- ::notice title=lint/suspicious/noExplicitAny,file=./src/app.tsx,line=9,endLine=9,col=64,endColumn=67::Unexpected any. Specify a different type.
+-- ::warning title=lint/suspicious/noEvolvingTypes,file=./src/app.tsx,line=10,endLine=10,col=7,endColumn=13::The type of this variable may evolve implicitly to any type, including the any type.
+-- ::error title=lint/suspicious/noImplicitAnyLet,file=./src/app.tsx,line=10,endLine=10,col=7,endColumn=13::This variable implicitly has the any type.
+
 local binary_name = "biome"
 
 return {
   cmd = function()
-    local local_binary = vim.fn.fnamemodify('./node_modules/.bin/' .. binary_name, ':p')
+    local local_binary
+
+    if vim.version.cmp(vim.version(), { 0, 10, 0 }) == 1 then
+      local rootDir = vim.fs.root(0, "node_modules")
+      local_binary = rootDir .. "/node_modules/.bin/" .. binary_name
+    else
+      local_binary =
+        vim.fn.fnamemodify("./node_modules/.bin/" .. binary_name, ":p")
+    end
+
     return vim.loop.fs_stat(local_binary) and local_binary or binary_name
   end,
-  args = { "lint" },
+  args = { "lint", "--reporter=github" },
   stdin = false,
   ignore_exitcode = true,
   stream = "both",
-  parser = function(output)
-    local diagnostics = {}
-
-
-    -- The diagnostic details we need are spread in the first 3 lines of
-    -- each error report.  These variables are declared out of the FOR
-    -- loop because we need to carry their values to parse multiple lines.
-    local fetch_message = false
-    local lnum, col, code, message
-
-    -- When a lnum:col:code line is detected fetch_message is set to true.
-    -- While fetch_message is true we will search for the error message.
-    -- When a error message is detected, we will create the diagnostic and
-    -- set fetch_message to false to restart the process and get the next
-    -- diagnostic.
-    for _, line in ipairs(vim.fn.split(output, "\n")) do
-      if fetch_message then
-        _, _, message = string.find(line, "%s×(.+)")
-
-        if message then
-          message = (message):gsub("^%s+×%s*", "")
-
-          table.insert(diagnostics, {
-            source = "biomejs",
-            lnum = tonumber(lnum) - 1,
-            col = tonumber(col),
-            message = message,
-            code = code
-          })
-
-          fetch_message = false
-        end
-      else
-        _, _, lnum, col, code = string.find(line, "[^:]+:(%d+):(%d+)%s([%a%/]+)")
-
-        if lnum then
-          fetch_message = true
-        end
-      end
-    end
-
-    return diagnostics
-  end
+  parser = require("lint.parser").from_pattern(
+    "::(.+) title=(.+),file=(.+),line=(%d+),endLine=(%d+),col=(%d+),endColumn=(%d+)::(.+)",
+    {
+      "severity",
+      "code",
+      "file",
+      "lnum",
+      "end_lnum",
+      "col",
+      "end_col",
+      "message",
+    },
+    {
+      ["error"] = vim.diagnostic.severity.ERROR,
+      ["warning"] = vim.diagnostic.severity.WARN,
+      ["notice"] = vim.diagnostic.severity.INFO,
+    },
+    { ["source"] = "biomejs" },
+    { lnum_offset = 0, end_lnum_offset = 0, end_col_offset = -1 }
+  ),
 }
