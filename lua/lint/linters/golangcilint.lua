292100c150522847c6628ebe435c4c76c8871b7b
Danthewaann <daniel.black@slicelife.com>
2025-07-08 08:05:14 +0100

    fix: use go env GOMOD to determine what filename modifier to use

--- lua/lint/linters/golangcilint.lua
+++ lua/lint/linters/golangcilint.lua
@@ -7,14 +7,28 @@ local severities = {
 
 -- Gets the correct arguments to run based on the version of golangci-lint
 local getArgs = function()
-  local ok, output = pcall(vim.fn.system, { 'golangci-lint', 'version' })
+  local ok, version_info = pcall(vim.fn.system, { 'golangci-lint', 'version' })
   if not ok then
     return
   end
 
+  -- Handle editing a standalone golang file that isn't apart of a normal setup where
+  -- the go.mod file can't be found by golangci-lint. In this situation, we need to provide
+  -- the current buffer path to golangci-lint instead of it's parent directory to allow
+  -- the buffer content to be linted, otherwise golangci-lint will raise an error
+  local ok, go_mod_location = pcall(vim.fn.system, { 'go', 'env', "GOMOD" })
+  if not ok then
+    return
+  end
+  local filename_modifier = ":h"
+  -- No go.mod file was found, so just lint the buffer directly
+  if go_mod_location:gsub("%s+", "") == "/dev/null" then
+    filename_modifier = ":p"
+  end
+
   -- The golangci-lint install script and prebuilt binaries strip the v from the version
   --   tag so both strings must be checked
-  if string.find(output, 'version v1') or string.find(output, 'version 1') then
+  if string.find(version_info, 'version v1') or string.find(version_info, 'version 1') then
     return {
       'run',
       '--out-format',
@@ -24,14 +38,14 @@ local getArgs = function()
       '--print-issued-lines=false',
       '--print-linter-name=false',
       function()
-        return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":p")
+        return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), filename_modifier)
       end
     }
   end
 
   -- Omit --path-mode=abs, as it was added on v2.1.0
   -- Make sure it won't break v2.0.{0,1,2}
-  if string.find(output, 'version v2.0.') or string.find(output, 'version 2.0.') then
+  if string.find(version_info, 'version v2.0.') or string.find(version_info, 'version 2.0.') then
     -- If the linter is not working as expected, users should explicitly add
     -- `run.relative-path-mode: wd` to their .golangci.yaml as a workaround to preserve the previous behavior.
     -- Prior to v2.0.0, the default for `run.relative-path-mode` was "wd".
@@ -52,7 +66,7 @@ local getArgs = function()
       '--issues-exit-code=0',
       '--show-stats=false',
       function()
-        return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":p")
+        return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), filename_modifier)
       end,
     }
   end
@@ -74,7 +88,7 @@ local getArgs = function()
     -- Get absolute path of the linted file
     '--path-mode=abs',
     function()
-      return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":p")
+      return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), filename_modifier)
     end,
   }
 end
