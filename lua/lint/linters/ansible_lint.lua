349b425e0f9ca2cf40c4c82f11f5d21a5ae6f47f
Sebastian Mark <smark+github@posteo.net>
2025-08-19 16:50:04 +0200

    fix(ansible-lint): update efm to match all output formats
    ansible-lint parseable output can be
    `%f:%l:%c: %m` and `%f:%l: %m`

--- lua/lint/linters/ansible_lint.lua
+++ lua/lint/linters/ansible_lint.lua
@@ -1,8 +1,9 @@
+local efm = '%f:%l:%c: %m,%f:%l: %m'
 return {
   cmd = 'ansible-lint',
   args = { '-p', '--nocolor' },
   ignore_exitcode = true,
-  parser = require('lint.parser').from_errorformat('%f:%l: %m', {
+  parser = require('lint.parser').from_errorformat(efm, {
     source = 'ansible-lint',
     severity = vim.diagnostic.severity.INFO
   })
