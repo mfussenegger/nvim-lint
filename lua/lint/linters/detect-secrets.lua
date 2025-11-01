3805137318e84dc24db3802633274aee0838eda5
Mathias Fussenegger <f.mathias@zignar.net>
2025-11-01 08:37:41 +0100

    fixup! Add detect-secrets linter

--- lua/lint/linters/detect-secrets.lua
+++ lua/lint/linters/detect-secrets.lua
@@ -1,12 +1,8 @@
--- Constants
-local LINTER_NAME = "detect-secrets"
-
+local name = "linter-secrets"
 return function()
-  ---Arguments to pass to detect-secrets
   local args = { "scan" }
 
-  ---Baseline file (use the baseline name in the example at https://github.com/Yelp/detect-secrets?tab=readme-ov-file#examples)
-  ---@type string
+  -- See https://github.com/Yelp/detect-secrets?tab=readme-ov-file#examples)
   local baseline = vim.fn.findfile(".secrets.baseline", ".;")
 
   if baseline ~= "" then
@@ -16,34 +12,25 @@ return function()
 
   ---@type lint.Linter
   return {
-    name = LINTER_NAME,
-    cmd = "detect-secrets",
+    name = name,
+    cmd = name,
     stdin = false,
     append_fname = true,
     args = args,
     stream = "stdout",
     ignore_exitcode = false,
-    parser = function(output, bufnr, _)
+    parser = function(output, _, _)
       local decoded_output = vim.json.decode(output)
-
-      ---Diagnostics generated with the output of detect-secrets
-      ---@type vim.Diagnostic[]
       local diagnostics = {}
 
       for _, leaks in pairs(decoded_output.results) do
         for _, leak in ipairs(leaks) do
-          -- 'detect-secrets' does not provide the line number, so we need to get it from the buffer
-          local line = vim.api.nvim_buf_get_lines(bufnr, leak.line_number - 1, leak.line_number, true)[1]
-
-          ---@type vim.Diagnostic
+          ---@type vim.Diagnostic.Set
           local new_diagnostic = {
-            bufnr = bufnr,
-            col = #line - 1,
-            source = LINTER_NAME,
+            source = name,
             lnum = leak.line_number - 1, -- 'detect-secrets' uses 1-indexed line numbers
             message = leak.type,
           }
-
           table.insert(diagnostics, new_diagnostic)
         end
       end
