9017a77a39e1873c975a84f9f8b0bc3cbc3d6377
Samad Khafi <skhafi7@gmail.com>
2025-11-17 11:40:41 +0530

    Add mago analyzer

---
+++ lua/lint/linters/mago_analyze.lua
@@ -0,0 +1,23 @@
+local severities = {
+  error = vim.diagnostic.severity.ERROR,
+  warning = vim.diagnostic.severity.WARN,
+  note = vim.diagnostic.severity.INFO,
+  help = vim.diagnostic.severity.HINT,
+}
+
+local pattern = '[^:]+:(%d+):(%d+):%s?(%l+)%[([%w-]+)%]:%s?(.+)'
+local groups = { 'lnum', 'col', 'severity', 'code', 'message' }
+
+return {
+  cmd = "mago",
+  args = { "--colors=never", "analyze", "--reporting-format=short" },
+  append_fname = true,
+  stdin = false,
+  ignore_exitcode = true,
+  parser = require('lint.parser').from_pattern(
+    pattern,
+    groups,
+    severities,
+    { ['source'] = 'mago_analyze' }
+  ),
+}
