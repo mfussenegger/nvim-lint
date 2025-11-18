b5988501009b56797e50aa9d9753b2cab0db56a5
Jannik Schäfer <jannik.schaefer@inbox-online.de>
2025-11-18 22:18:39 +0100

    mado: Support stdin

--- lua/lint/linters/mado.lua
+++ lua/lint/linters/mado.lua
@@ -1,11 +1,11 @@
-local pattern = "([^:]+):(%d+):(%d+):%s*([A-Z%d]+)%s+(.+)"
-local groups = { "file", "lnum", "col", "code", "message" }
+local pattern = "%(stdin%):(%d+):(%d+):%s*([A-Z%d]+)%s+(.+)"
+local groups = { "lnum", "col", "code", "message" }
 local defaults = { ["source"] = "mado", ["severity"] = vim.diagnostic.severity.WARN }
 
 return {
   cmd = "mado",
   args = { "check", "--quiet" },
-  stdin = false,
+  stdin = true,
   stream = "stdout",
   ignore_exitcode = true,
   parser = require("lint.parser").from_pattern(pattern, groups, nil, defaults),
