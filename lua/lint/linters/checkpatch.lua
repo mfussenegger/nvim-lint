661786ab19a238ccf8d35d5ced3cdcfed5c2a568
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2025-11-19 22:58:38 -0800

    checkpatch: favor version of the script supplied by projects
    Linux kernel is not the only project that uses checkpatch.pl for patch
    validation, other projects (for example qemu) use it too, and their
    version is different from the one used by the kernel. Also checkpatch.pl
    changes from kernel version to kernel version when working with multiple
    versions, so requiring a single version covering everything is not
    practical.
    
    Additionally running checkpatch.pl on projects that do not use it and do
    not use kernel coding style is counter-productive.
    
    To solve this issue make the linter locate and favor the version of
    checkpatch.pl belonging to the project that file being linted belongs
    to. Also, if checkpatch.pl is not accessible use "true" as the command
    so that there are no errors/notifications and no diagnostic.

--- lua/lint/linters/checkpatch.lua
+++ lua/lint/linters/checkpatch.lua
@@ -1,6 +1,10 @@
--- The scripts/ directory of the Linux kernel tree needs to be in your PATH or
--- the full path to the checkpatch.pl script can be overriden from user config:
+-- This favors the version of `checkpatch.pl` supplied by the project
+-- to which the file being linted belongs, falling back to a version
+-- accessible via `$PATH`. If a user wishes to use a different version
+-- of the script they can override "cmd" in their config:
 -- require("lint").linters.checkpatch.cmd = '…/checkpatch.pl'
+
+local tool_name = 'checkpatch.pl'
 
 -- path/to/file:line: severity: message
 local pattern = '([^:]+):(%d+): (%a+): (.+)'
@@ -11,8 +15,25 @@ local severity_map = {
   ['CHECK'] = vim.diagnostic.severity.INFO,
 }
 
+local function locate_checkpatch()
+  local tool_path = vim.fs.joinpath(vim.fn.getcwd(), 'scripts', tool_name)
+  if vim.fn.executable(tool_path) == 1 then
+    return tool_path
+  end
+
+  if vim.fn.executable(tool_name) == 1 then
+    return tool_name
+  end
+
+  -- If checkpatch executable is not found return "true" which is a valid command
+  -- that will not produce any diagnostic but return success error code. This will
+  -- allow enabling checkpatch linter globally and only return diagnostic if
+  -- a project is actually using it.
+  return 'true'
+end
+
 return {
-  cmd = 'checkpatch.pl',
+  cmd = locate_checkpatch,
   stdin = false,
   args = {
     '--strict',
