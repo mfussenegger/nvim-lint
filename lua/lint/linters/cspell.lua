b567f78b8eca723ff224ce7fead8022e0cfc5b72
Ya Qia <yyaqia@gmail.com>
2025-12-16 03:36:23 +0800

    chore(cspell): revert the formating stuff

--- lua/lint/linters/cspell.lua
+++ lua/lint/linters/cspell.lua
@@ -1,34 +1,34 @@
-local efm = "%f:%l:%c - %m"
+local efm = '%f:%l:%c - %m'
 return {
-  cmd = "cspell",
+  cmd = 'cspell',
   ignore_exitcode = true,
   args = {
-    "lint",
-    "--no-color",
-    "--no-progress",
-    "--no-summary",
+    'lint',
+    '--no-color',
+    '--no-progress',
+    '--no-summary',
     function()
-      return "stdin://" .. vim.api.nvim_buf_get_name(0)
+      return 'stdin://' .. vim.api.nvim_buf_get_name(0)
     end,
   },
   stdin = true,
-  stream = "stdout",
+  stream = 'stdout',
   parser = function(output)
-    local lines = vim.split(output, "\n")
+    local lines = vim.split(output, '\n')
     local qflist = vim.fn.getqflist({ efm = efm, lines = lines })
     local result = {}
     for _, item in pairs(qflist.items) do
       if item.valid == 1 then
-        local message = item.text:match("^%s*(.-)%s*$")
-        local word = message:match("%(.*%)")
+        local message = item.text:match('^%s*(.-)%s*$')
+        local word = message:match('%(.*%)')
         local lnum = math.max(0, item.lnum - 1)
         local line_text = vim.api.nvim_buf_get_lines(0, lnum, lnum + 1, false)[1]
         if line_text then
           local col_char = math.max(0, item.col - 1)
           local end_lnum = item.end_lnum > 0 and (item.end_lnum - 1) or lnum
           local col_byte, end_col_byte
-          if vim.fn.has("nvim-0.11") == 1 then
-            col_byte = vim.str_byteindex(line_text, "utf-16", col_char, false)
+          if vim.fn.has('nvim-0.11') == 1 then
+            col_byte = vim.str_byteindex(line_text, 'utf-16', col_char, false)
           else
             col_byte = vim.str_byteindex(line_text, col_char, true)
           end
@@ -39,7 +39,7 @@ return {
             end_lnum = end_lnum,
             end_col = end_col_byte,
             message = message,
-            source = "cspell",
+            source = 'cspell',
             severity = vim.diagnostic.severity.INFO,
           }
           table.insert(result, diagnostic)
@@ -47,5 +47,5 @@ return {
       end
     end
     return result
-  end,
+  end
 }
