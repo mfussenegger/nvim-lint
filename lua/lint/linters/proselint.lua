3d78d70281fc0375a0cf427a0a9aa08b3afd626d
Moshe Avni <moshe.avni@flexera.com>
2025-12-28 17:07:58 +0200

    fix/proselint-json

--- lua/lint/linters/proselint.lua
+++ lua/lint/linters/proselint.lua
@@ -1,14 +1,29 @@
--- path/to/file:line:col: code.subcode.subsubcode message
-local pattern = '([^:]+):(%d+):(%d+): ([^ ]+) (.*)'
-local groups = { 'file', 'lnum', 'col', 'code', 'message' }
-
 return {
   cmd = 'proselint',
   stdin = false,
-  args = {},
+  args = { 'check', '--output-format=json' },
   ignore_exitcode = true,
-  parser = require('lint.parser').from_pattern(pattern, groups, nil, {
-    ['source'] = 'proselint',
-    ['severity'] = vim.diagnostic.severity.INFO,
-  }),
+  parser = function(output)
+    if output == '' then
+      return {}
+    end
+    local json_output = vim.json.decode(output, { luanil = { object = true } })
+    local diagnostics = {}
+    local file_key = vim.tbl_keys(json_output.result)[1]
+    local results = json_output.result[file_key].diagnostics
+    if results == nil then
+      return diagnostics
+    end
+    for _, item in ipairs(results) do
+      table.insert(diagnostics, {
+        lnum = item.pos[1] - 1,
+        col = item.pos[2] - 1,
+        message = item.message,
+        file = file_key,
+        code = item.check_path,
+        severity = vim.diagnostic.severity.INFO,
+      })
+    end
+    return diagnostics
+  end,
 }
