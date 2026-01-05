78ca2c2484d6b1d928f15959d3f5aa5e51fe59d4
Maik Wramba <mail@mkwa.dev>
2026-01-05 23:25:01 +0000

    markdownlint-cli2: support standard input

--- lua/lint/linters/markdownlint-cli2.lua
+++ lua/lint/linters/markdownlint-cli2.lua
@@ -1,6 +1,8 @@
-local efm = "%f:%l:%c %m,%f:%l %m"
+local efm = "stdin:%l:%c %m,stdin:%l %m"
 return {
   cmd = "markdownlint-cli2",
+  stdin = true,
+  args = { "-" },
   ignore_exitcode = true,
   stream = "stderr",
   parser = require("lint.parser").from_errorformat(efm, {
